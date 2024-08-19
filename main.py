cad362c8ef96fe41cd755a73afcff823d274e4e8
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2024-08-19 16:19:11 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import streamlit as st
+import mysql.connector
 
-# Initialize connection using the details from the secrets.toml
-conn = st.connection('mysql', type='sql', 
-                     host=st.secrets["connections"]["mysql"]["host"], 
-                     port=st.secrets["connections"]["mysql"]["port"],
-                     database=st.secrets["connections"]["mysql"]["database"],
-                     username=st.secrets["connections"]["mysql"]["username"], 
-                     password=st.secrets["connections"]["mysql"]["password"])
+# Initialize connection using mysql-connector-python
+conn = mysql.connector.connect(
+    host=st.secrets["connections"]["mysql"]["host"],
+    username=st.secrets["connections"]["mysql"]["username"],
+    password=st.secrets["connections"]["mysql"]["password"],
+    database=st.secrets["connections"]["mysql"]["database"],
+    port=st.secrets["connections"]["mysql"]["port"]
+)
 
 # Perform query
-df = conn.query('SELECT * from pet_info;', ttl=600)
+cursor = conn.cursor()
+cursor.execute('SELECT * from pet_info;')
+rows = cursor.fetchall()
 
 # Print results
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
