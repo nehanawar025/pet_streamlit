4831086eff3d470fb8a5da918cbba252e1e83c2f
Noshin Nawar Neha <99127699+nehanawar025@users.noreply.github.com>
2024-08-19 16:56:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
+# import streamlit as st
+# import mysql.connector
+
+# # Initialize connection using mysql-connector-python
+# conn = mysql.connector.connect(
+#     host=st.secrets["connections"]["mysql"]["host"],
+#     username=st.secrets["connections"]["mysql"]["username"],
+#     password=st.secrets["connections"]["mysql"]["password"],
+#     database=st.secrets["connections"]["mysql"]["database"],
+#     port=st.secrets["connections"]["mysql"]["port"]
+# )
+
+# # Perform query
+# cursor = conn.cursor()
+# cursor.execute('SELECT * from pet_info;')
+# rows = cursor.fetchall()
+
+# # Print results
+# for row in rows:
+#     st.write(f"{row[0]} has a :{row[1]}:")
+
+
 import streamlit as st
-import mysql.connector
 
-# Initialize connection using mysql-connector-python
-conn = mysql.connector.connect(
-    host=st.secrets["connections"]["mysql"]["host"],
-    username=st.secrets["connections"]["mysql"]["username"],
-    password=st.secrets["connections"]["mysql"]["password"],
-    database=st.secrets["connections"]["mysql"]["database"],
-    port=st.secrets["connections"]["mysql"]["port"]
-)
+# Initialize connection.
+conn = st.connection('mysql', type='sql')
 
-# Perform query
-cursor = conn.cursor()
-cursor.execute('SELECT * from pet_info;')
-rows = cursor.fetchall()
+# Perform query.
+df = conn.query('SELECT * from pet_info;', ttl=600)
 
-# Print results
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
+
+
